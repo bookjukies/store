eb36982eaa443bec0dc053aed7dee5430fe54d01
bongani <mkhumbie34@gmail.com>
2023-01-06 03:25:53 +0200

    updated ports for deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app =  express()
-const port = 5000
+const port = process.env.PORT || 5000
 const connection = require('./database/connect')
 const session = require('./middleware/session')
 const passport = require('passport');
